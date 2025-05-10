3dfd61213319de4979ad2c0688f8c42cf696132a
mausmee123 <mausmee.bvminfotech@gmail.com>
2025-05-10 22:45:08 +0530

    first milestone

---
+++ API/server.js
@@ -0,0 +1,36 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from 'express'
+import userRouter from './routes/user.js'
+import recipeRouter from './routes/recipe.js'
+import cors from 'cors'
+const app = express(); 
+
+app.use(bodyParser.json())
+app.use(cors({
+  origin:true,
+  methods:["GET","POST","PUT","DELETE"],
+  credentials:true
+ 
+}))
+
+// userRouter
+app.use('/api',userRouter)
+
+// recipeRouter
+app.use('/api',recipeRouter)
+
+mongoose
+  .connect(
+    "mongodb+srv://recipe-demo:<db_password>@cluster0.7stlvw7.mongodb.net/",
+    {
+      dbName: "recipe-demo",
+    }
+  )
+  .then(() => console.log("MongoDB is Connected..!"))
+  .catch((err) => console.log(err.message));
+
+const port = 3000;
+app.listen(port, () => console.log(`server is running on port ${port}`));
+
+
